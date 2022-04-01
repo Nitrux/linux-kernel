7999aa48ffa4ab984fe3e7a5ec3a433c9bd55214
Uri Herrera <hello@uriherrera.com>
2022-03-31 22:36:27 -0600

    create pkg

--- travis-build.sh
+++ travis-build.sh
@@ -10,8 +10,8 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: linux-image-xanmod" \
-	"Version: 0.3.1-$git_commit" \
+	"Package: linux-image-xanmod-edge" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: " \
