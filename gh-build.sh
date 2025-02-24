4258e628081421fa5bf7e350a441a7271d2f985b
Uri Herrera <hello@uriherrera.com>
2025-02-24 11:34:28 -0600

    create pkg

--- gh-build.sh
+++ gh-build.sh
@@ -10,11 +10,11 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: linux-image-liquorix" \
+	"Package: linux-image-pikaos" \
 	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: amd64" \
-	"Description: Liquorix is a distro kernel replacement built using the best configuration."
+	"Description: PikaOS Linux kernel and headers packages with patches from CachyOS."
 
 equivs-build configuration
